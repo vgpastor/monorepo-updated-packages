02da6d533a90af945a987fb6946d0a2f4cb8565a
Víctor García <vgpastor08@gmail.com>
2023-01-11 14:36:43 +0100

    First edition

---
+++ src/infrastructure/githubApi.ts
@@ -0,0 +1,67 @@
+import * as core from '@actions/core'
+import {context, getOctokit} from '@actions/github'
+import type {GitHub} from '@actions/github/lib/utils'
+export default class GithubApi {
+  private client: InstanceType<typeof GitHub>
+  constructor(private readonly token: string) {
+    this.client = getOctokit(token)
+  }
+
+  async getModifiedFiles(base: string, head: string) {
+    // Debug log the payload.
+    core.debug(`Payload keys: ${Object.keys(context.payload)}`)
+
+    // Log the base and head commits
+    core.info(`Base commit: ${base}`)
+    core.info(`Head commit: ${head}`)
+
+    // Ensure that the base and head properties are set on the payload.
+    if (!base || !head) {
+      core.setFailed(
+        `The base and head commits are missing from the payload for this ${context.eventName} event. ` +
+          "Please submit an issue on this action's GitHub repo."
+      )
+      // To satisfy TypeScript, even though this is unreachable.
+    }
+
+    // https://developer.github.com/v3/repos/commits/#compare-two-commits
+    this.client
+      .request('GET /repos/{owner}/{repo}/compare/{base}...{head}', {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        base,
+        head
+      })
+      .then(response => {
+        core.debug(`Response: ${response}`)
+
+        if (response.status !== 200) {
+          core.setFailed(
+            `The GitHub API for comparing the base and head commits for this ${context.eventName} event returned ${response.status}, expected 200. ` +
+              "Please submit an issue on this action's GitHub repo."
+          )
+        }
+
+        // Ensure that the head commit is ahead of the base commit.
+        if (response.data.status !== 'ahead') {
+          core.setFailed(
+            `The head commit for this ${context.eventName} event is not ahead of the base commit. ` +
+              "Please submit an issue on this action's GitHub repo."
+          )
+        }
+
+        // Get the changed files from the response payload.
+        return response.data.files
+      })
+    // Use GitHub's compare two commits API.
+    // https://developer.github.com/v3/repos/commits/#compare-two-commits
+    // const response = await this.client.repos.compareCommits({
+    //   base,
+    //   head,
+    //   owner: context.repo.owner,
+    //   repo: context.repo.repo
+    // })
+
+    // Ensure that the request was successful.
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,38 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {context} from '@actions/github'
+import GithubApi from './infrastructure/githubApi'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const folder = core.getInput('folder', {required: true})
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const githubApi = new GithubApi(core.getInput('token', {required: true}))
 
-    core.setOutput('time', new Date().toTimeString())
+    // Define the base and head commits to be extracted from the payload.
+    let base = ''
+    let head = ''
+
+    switch (context.eventName) {
+      case 'pull_request':
+        base = context.payload.pull_request?.base?.sha
+        head = context.payload.pull_request?.head?.sha
+        break
+      case 'push':
+        base = context.payload.before
+        head = context.payload.after
+        break
+      default:
+        core.setFailed(
+          `This action only supports pull requests and pushes, ${context.eventName} events are not supported. ` +
+            "Please submit an issue on this action's GitHub repo if you believe this in correct."
+        )
+    }
+
+    let filesModified = githubApi.getModifiedFiles(base, head)
+
+    filesModified.then(files => {
+      core.info(`Files modified: ${files}`)
+    })
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
