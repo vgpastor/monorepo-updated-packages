7008d96f72fb38edb4023d8f6bddd97e4d40e0b4
Víctor García <vgpastor08@gmail.com>
2023-01-11 15:38:08 +0100

    Update version

--- src/infrastructure/githuba-api.ts
+++ src/infrastructure/githuba-api.ts
@@ -26,38 +26,39 @@ export default class GithubApi {
     }
 
     // https://developer.github.com/v3/repos/commits/#compare-two-commits
-    this.client
-      .request('GET /repos/{owner}/{repo}/compare/{base}...{head}', {
+    return this.client.request(
+      'GET /repos/{owner}/{repo}/compare/{base}...{head}',
+      {
         owner: context.repo.owner,
         repo: context.repo.repo,
         base,
         head
-      })
-      // eslint-disable-next-line github/no-then
-      .then(response => {
-        core.debug(`Response: ${response}`)
-        core.debug(`Response data: ${response.data}`)
-        core.debug(`Response data files: ${response.data.files}`)
-        core.debug(`Response status: ${response.status}`)
-
-        if (response.status !== 200) {
-          core.setFailed(
-            `The GitHub API for comparing the base and head commits for this ${context.eventName} event returned ${response.status}, expected 200. ` +
-              "Please submit an issue on this action's GitHub repo."
-          )
-        }
-
-        // Ensure that the head commit is ahead of the base commit.
-        if (response.data.status !== 'ahead') {
-          core.setFailed(
-            `The head commit for this ${context.eventName} event is not ahead of the base commit. ` +
-              "Please submit an issue on this action's GitHub repo."
-          )
-        }
-
-        // Get the changed files from the response payload.
-        return response.data.files
-      })
+      }
+    )
+    // .then(response => {
+    //   core.debug(`Response: ${response}`)
+    //   core.debug(`Response data: ${response.data}`)
+    //   core.debug(`Response data files: ${response.data.files}`)
+    //   core.debug(`Response status: ${response.status}`)
+    //
+    //   if (response.status !== 200) {
+    //     core.setFailed(
+    //       `The GitHub API for comparing the base and head commits for this ${context.eventName} event returned ${response.status}, expected 200. ` +
+    //         "Please submit an issue on this action's GitHub repo."
+    //     )
+    //   }
+    //
+    //   // Ensure that the head commit is ahead of the base commit.
+    //   if (response.data.status !== 'ahead') {
+    //     core.setFailed(
+    //       `The head commit for this ${context.eventName} event is not ahead of the base commit. ` +
+    //         "Please submit an issue on this action's GitHub repo."
+    //     )
+    //   }
+    //
+    //   // Get the changed files from the response payload.
+    //   return response.data.files
+    // })
     // Use GitHub's compare two commits API.
     // https://developer.github.com/v3/repos/commits/#compare-two-commits
     // const response = await this.client.repos.compareCommits({
